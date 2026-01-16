b3e298d68c23a599a478827ce201cb0028ceb6a2
Amra <amra51528@gmail.com>
2026-01-17 00:20:30 +0100

    Initial project upload

---
+++ src/test/java/com/example/notimdb/NotimdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notimdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotimdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
